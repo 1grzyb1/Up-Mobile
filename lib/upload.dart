a6bfff2f3ab55dc7655c8dc85c415c7d20075ca4
grzybek <grzybol.k@gmail.com>
2020-07-14 22:20:30 +0200

    Notification

--- lib/upload.dart
+++ lib/upload.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:ui';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:path/path.dart' as path;
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_uploader/flutter_uploader.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:upflutter/Model/ListItem.dart';
@@ -32,7 +35,10 @@ class UploadPage extends State<MyHomePage> {
   final url = "https://up.snet.ovh/";
   final uploader = FlutterUploader();
   StreamSubscription _intentDataStreamSubscription;
-  String _sharedText;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+  var initializationSettingsAndroid;
+  var initializationSettingsIOS;
+  var initializationSettings;
 
   String get _fileName {
     return file.path.split("/").last;
@@ -42,6 +48,11 @@ class UploadPage extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    // Init notification settings
+    initializationSettingsAndroid = new AndroidInitializationSettings('app_icon');
+    initializationSettingsIOS = new IOSInitializationSettings( );
+    initializationSettings = new InitializationSettings(initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification);
     // Init history
     SharedPreferences.getInstance().then((SharedPreferences sp) {
       prefs = sp;
@@ -85,8 +96,14 @@ class UploadPage extends State<MyHomePage> {
     // For sharing or opening urls/text coming from outside the app while the app is in the memory
     _intentDataStreamSubscription =
         ReceiveSharingIntent.getTextStream().listen((value) {
-          setState(() {
-            String _sharedText = value;
+          setState(() async {
+            if(value == null) return;
+            Directory tempDir=await getTemporaryDirectory();
+            String tempPath = tempDir.path;
+            file = new File('$tempPath/file.txt');
+            file.writeAsString(value);
+            selectedFile="Clipboard paste";
+            uploadFile(file, delAfter: true);
           });
         }, onError: (err) {
           print("getLinkStream error: $err");
@@ -94,8 +111,14 @@ class UploadPage extends State<MyHomePage> {
 
     // For sharing or opening urls/text coming from outside the app while the app is closed
     ReceiveSharingIntent.getInitialText().then((String value) {
-      setState(() {
-        String sharedText = value;
+      setState(() async {
+        if(value == null) return;
+        Directory tempDir=await getTemporaryDirectory();
+        String tempPath = tempDir.path;
+        file = new File('$tempPath/file.txt');
+        file.writeAsString(value);
+        selectedFile="Clipboard paste";
+        uploadFile(file, delAfter: true);
       });
     });
   }
@@ -198,7 +221,7 @@ class UploadPage extends State<MyHomePage> {
   }
 
   /// Upload file
-  Future<void> uploadFile(File fileToUpload) async {
+  Future<void> uploadFile(File fileToUpload, {bool delAfter=false}) async {
     // Listen to progress of upload
     uploader.progress.listen((progress) {
       setState(() {
@@ -216,7 +239,7 @@ class UploadPage extends State<MyHomePage> {
             fieldname: "file")
       ],
       method: UploadMethod.POST,
-      showNotification: true,
+      showNotification: false,
       tag: "upload",
     );
     // listen to result of upload
@@ -230,9 +253,11 @@ class UploadPage extends State<MyHomePage> {
               DateTime.now().millisecondsSinceEpoch, json["toDelete"], link);
           historyItems.add(historyItem);
         }
+        notification("Uploaded", selectedFile, link);
         selectedFile = null;
         SharedPreferences prefs = await SharedPreferences.getInstance();
         prefs.setString("history", jsonEncode(historyItems));
+        if(delAfter) file.delete();
       });
     }, onError: (ex, stacktrace) {
       Fluttertoast.showToast(
@@ -250,5 +275,30 @@ class UploadPage extends State<MyHomePage> {
   void cancel() {
     selectedFile = null;
     uploader.cancelAll();
+  }
+
+  Future notification(String title, String body, String link) async{
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0, title, body, platformChannelSpecifics,
+        payload: link,
+    );
+  }
+
+  Future onSelectNotification(String payload) async {
+    Clipboard.setData(ClipboardData(text: payload));
+    Fluttertoast.showToast(
+    msg: "Link coppied",
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 1,
+    backgroundColor: Colors.grey,
+    textColor: Colors.white,
+    fontSize: 16.0);
   }
 }
